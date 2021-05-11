3ef2a026fa737b72dccc5bea6b1bce18267ef370
walidoc <walidrahali10@gmail.com>
2021-05-11 14:07:21 +0000

    add helper scripts

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="walidoc/mlapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapp\
+    --image=$dockerpath\
+    --port=80 --labels app=mlapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlapp 8000:80
